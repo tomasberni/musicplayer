4027cd42c594b36796d6a17a2d33189fffba4c91
T0MAS2 <tomasbernimozzo@gmail.com>
2023-01-17 14:25:48 +0000

    msj

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    //aca los useState
+	
+    const [cancion, setcancion] =useState([]);
+	let [lugar, setlugar]= useState(0);
+	let play = useRef(0);
 
-export default Home;
+    useEffect(()=>{
+    fetch('https://assets.breatheco.de/apis/sound/songs') 
+    .then((response)=>response.json()) 
+    .then((data)=>setcancion(data)) 
+    },[])
+
+	function playMusic(url, index){
+		if(play.current.paused){
+			play.current.src= `https://assets.breatheco.de/apis/sound/${url}`
+			play.current.play()
+
+		} else {
+			play.current.pause
+		}
+		setlugar(index)
+	}
+
+	function playNext(){
+		setlugar(lugar ++)
+		play.current.src=`https://assets.breatheco.de/apis/sound/${cancion[lugar].url}`
+		play.current.play()
+	}
+	function playBack(){
+		setlugar(lugar --)
+		play.current.src=`https://assets.breatheco.de/apis/sound/${cancion[lugar].url}`
+		play.current.play()
+	}
+
+	return (
+         <>
+			<div className="container w-50 m-3 p-3">
+				<div className="list-group bg-dark rounded-0">
+					{cancion.map((item,index)=><button className="btn btn-dark text-start rounded-0" onClick={() => playMusic(item.url,index)} type="button" key={index}>{index} {item.name}</button>)}
+				</div>
+				<div className="d-flex justify-content-center bg-dark border-top">
+					<button onClick={playBack}><i className="fa fa-backward"></i></button>
+					<audio ref={play} controls/>
+					<button onClick={playNext}><i className="fa fa-forward"></i></button>
+				</div>
+			</div>
+        </>
+    );
+};
+    
+    export default Home;
